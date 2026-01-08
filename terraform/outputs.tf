59a43da709d2f8ddee3b6cd9b0cf2acad02bae8e
Adrien Quacchia <777adrien@gmail.com>
2026-01-08 04:54:27 +0100

    feat: deploy nextjs frotnend with ansible and terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,9 @@
-output "france_dev_ip" {
-  value = module.france_dev.instance_ip
+output "france_dev_backend_ip" {
+  value = module.france_dev.backend_instance_ip
+}
+
+output "france_dev_frontend_ip" {
+  value = module.france_dev.frontend_instance_ip
 }
 
 output "france_staging_ip" {
@@ -24,12 +28,13 @@ output "germany_prod_ip" {
 
 output "ssh_private_keys" {
   value = {
-    france_dev     = module.france_dev.ssh_private_key
-    germany_dev    = module.germany_dev.ssh_private_key
-    france_staging = module.france_staging.ssh_private_key
-    germany_staging = module.germany_staging.ssh_private_key
-    france_prod    = module.france_prod.ssh_private_key
-    germany_prod   = module.germany_prod.ssh_private_key
+    france_dev_backend  = module.france_dev.backend_ssh_private_key
+    france_dev_frontend = module.france_dev.frontend_ssh_private_key
+    germany_dev         = module.germany_dev.ssh_private_key
+    france_staging      = module.france_staging.ssh_private_key
+    germany_staging     = module.germany_staging.ssh_private_key
+    france_prod         = module.france_prod.ssh_private_key
+    germany_prod        = module.germany_prod.ssh_private_key
   }
   sensitive = true
 }
